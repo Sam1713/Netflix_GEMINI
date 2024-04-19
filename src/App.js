ea60f26967e535799855628e06d7863074c8cc01
Sam1713 <samsaju399@gmail.com>
2024-04-19 11:43:30 +0530

    changes added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App m-auto">
+          <h1 className="text-3xl bg-yellow-400 underline w-6/12 text-center ">
+      Hello world!
+    </h1>
+    </div>
+  );
+}
+
+export default App;
